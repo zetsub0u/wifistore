ab40d03506c6de4a72ab83c0a036e87b055670b4
Alejandro <tovarich@gmail.com>
2013-07-10 15:43:53 -0400

    find nearest points

--- controller.js
+++ controller.js
@@ -5,6 +5,22 @@ app.get('/', function(req, res) {
     res.render('index.jade');
 });
 
+app.get('/findNear', function(req, res) {
+    var lat = req.query.lat;
+    var lon = req.query.lon;
+    model.AccessPoint.find({
+        geo: {
+            $near: [Number(lon), Number(lat)],
+            $maxDistance: 0.01
+        }
+    },
+
+    function(error, aps) {
+        console.log(aps);
+        res.send(aps);
+    });
+});
+
 app.get('/accesspoint', function(req, res) {
     var storeName = req.query.name;
     var ssid = req.query.ssid;
@@ -21,27 +37,30 @@ app.get('/accesspoint', function(req, res) {
         //console.log(dbUser);
         if (user != []) {
             user = dbUser;
-        } else {
+        }
+        else {
             res.send("not found");
             return;
         }
     });
-    
+
     var ap = new model.AccessPoint({
         name: storeName,
         ssid: ssid,
         password: password,
-        geo: [Number(lon),Number(lat)],
+        geo: [Number(lon), Number(lat)],
         updates: [{
             user_name: userName,
             when: Date.now()
         }]
     });
     ap.save(function(err, ap) {
-        if (err) { console.log('error: ' + err ) }
+        if (err) {
+            console.log('error: ' + err);
+        }
         //console.log(ap);
     });
-    
+
     res.send('Done!');
 });
 
@@ -52,7 +71,9 @@ app.get('/user', function(req, res) {
         password: 'pass1'
     });
     user.save(function(err, user) {
-        if (err) { console.log('error: ' + err ) }
+        if (err) {
+            console.log('error: ' + err);
+        }
         console.log("user saved");
     });
     res.send('User Created');
