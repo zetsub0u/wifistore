db7f7d078bba2949654a9f1d66a26055466b9dba
Alejandro <tovarich@gmail.com>
2013-07-10 17:45:47 -0400

    draw markers from db on map

--- public/js.js
+++ public/js.js
@@ -6,35 +6,51 @@ $(function() {
 
     //var myLatlng = new google.maps.LatLng(-25.363882, 131.044922);
     var mapOptions = {
-        zoom: 4,
+        zoom: 15,
         panControl: false,
-        zoomControl: false,
+        zoomControl: true,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-    var contentString = "Who could live in this shit place...";
+    var contentString = "You are here.";
 
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+    var userPos;
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = new google.maps.LatLng(position.coords.latitude,
+            userPos = new google.maps.LatLng(position.coords.latitude,
             position.coords.longitude);
-
-            var infowindow = new google.maps.InfoWindow({
+            //userPos = new google.maps.latlng(-34.595808,-58.485160);
+            
+            var locInfo = new google.maps.InfoWindow({
                 content: contentString
             });
-
-            var marker = new google.maps.Marker({
-                position: pos,
+            
+            var locMarker = new google.maps.Marker({
+                position: userPos,
                 map: map,
-                title: 'Dumpster'
+                title: 'Your Location'
             });
-
-            map.setCenter(pos);
-
-            google.maps.event.addListener(marker, 'click', function() {
+            var latlon = userPos.toUrlValue().split(',');
+            findNear(latlon[0], latlon[1]);
+            
+            map.setCenter(userPos);
+            
+            google.maps.event.addListener(locMarker, 'click', function() {
                 infowindow.open(map, marker);
+            });
+            var update_timeout;
+            google.maps.event.addListener(map, 'click', function(event) {
+                update_timeout = setTimeout(function(){
+                    locMarker.setPosition(event.latLng);
+                    var latlon = event.latLng.toUrlValue().split(',');
+                    findNear(latlon[0], latlon[1]);
+                }, 400);  
+            });
+            google.maps.event.addListener(map, 'dblclick', function(event) {       
+                clearTimeout(update_timeout);
             });
         }, function() {
             handleNoGeolocation(true);
@@ -44,4 +60,28 @@ $(function() {
         // Browser doesn't support Geolocation
         handleNoGeolocation(false);
     }
+
+    function findNear(lat, lon) {
+        $.getJSON('/findNear?lat=' + lat + '&lon=' + lon, function(data) {
+            console.log(data.length)
+            if (data.length > 0) {
+                console.log(data)
+                $.each(data, function(i) {
+                    console.log(data[i]);
+                    var infowindow = new google.maps.InfoWindow({
+                        content: "name: " + data[i].name
+                    });
+                    var marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(data[i].geo[1], data[i].geo[0]),
+                        map: map,
+                        title: "name: " + data[i].name
+                    });
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.open(map, marker);
+                    });
+                });
+            }
+        });
+    }
+
 });
