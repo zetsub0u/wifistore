133bc683b2b497b2f6e97535435e49f3e8f4e4aa
Alejandro <tovarich@gmail.com>
2013-07-11 12:16:42 -0400

    map imprvovement

--- public/js.js
+++ public/js.js
@@ -12,6 +12,8 @@ $(function() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
+    var markers = [];
+
     var contentString = "You are here.";
 
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -23,33 +25,35 @@ $(function() {
             userPos = new google.maps.LatLng(position.coords.latitude,
             position.coords.longitude);
             //userPos = new google.maps.latlng(-34.595808,-58.485160);
-            
+
             var locInfo = new google.maps.InfoWindow({
                 content: contentString
             });
-            
+
             var locMarker = new google.maps.Marker({
                 position: userPos,
                 map: map,
-                title: 'Your Location'
+                title: 'Your Location',
+                animation: google.maps.Animation.BOUNCE
             });
             var latlon = userPos.toUrlValue().split(',');
             findNear(latlon[0], latlon[1]);
-            
+
             map.setCenter(userPos);
-            
+
             google.maps.event.addListener(locMarker, 'click', function() {
                 infowindow.open(map, marker);
             });
             var update_timeout;
             google.maps.event.addListener(map, 'click', function(event) {
-                update_timeout = setTimeout(function(){
+                update_timeout = setTimeout(function() {
+                    clearMarkers();
                     locMarker.setPosition(event.latLng);
                     var latlon = event.latLng.toUrlValue().split(',');
                     findNear(latlon[0], latlon[1]);
-                }, 400);  
+                }, 400);
             });
-            google.maps.event.addListener(map, 'dblclick', function(event) {       
+            google.maps.event.addListener(map, 'dblclick', function(event) {
                 clearTimeout(update_timeout);
             });
         }, function() {
@@ -61,21 +65,31 @@ $(function() {
         handleNoGeolocation(false);
     }
 
+    function clearMarkers() {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(null);
+        }
+        markers = [];
+    }
+
+    function addMarker(location) {
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+        markers.push(marker);
+        return marker;
+    }
+
     function findNear(lat, lon) {
         $.getJSON('/findNear?lat=' + lat + '&lon=' + lon, function(data) {
             console.log(data.length)
             if (data.length > 0) {
-                console.log(data)
                 $.each(data, function(i) {
-                    console.log(data[i]);
                     var infowindow = new google.maps.InfoWindow({
                         content: "name: " + data[i].name
                     });
-                    var marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(data[i].geo[1], data[i].geo[0]),
-                        map: map,
-                        title: "name: " + data[i].name
-                    });
+                    var marker = addMarker(new google.maps.LatLng(data[i].geo[1], data[i].geo[0]))
                     google.maps.event.addListener(marker, 'click', function() {
                         infowindow.open(map, marker);
                     });
